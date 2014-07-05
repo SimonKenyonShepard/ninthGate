48393d068a050f08d411dae8eaf345eef38a99a1
Shepard Simon <simshepa@lx9902047.fritz.box>
2014-07-05 11:11:10 +0200

    test code

--- peerServer.js
+++ peerServer.js
@@ -2,3 +2,4 @@ var peerPort = process.env.PORT || 9000;
 
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({port: peerPort, path: '/myapp'});
+console.log("server created");
