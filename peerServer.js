5ed345a08ef0b8fa0a2d65b111e55f3e41a0df25
Shepard Simon <simshepa@lx9902047.fritz.box>
2014-07-05 11:43:57 +0200

    removing legacy copy and paste

--- peerServer.js
+++ peerServer.js
@@ -1,6 +1,3 @@
 var peerPort = process.env.PORT || 9000;
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({port: peerPort, path: '/'});
-setInterval(function() {
-	console.log(server);
-}, 1000);
